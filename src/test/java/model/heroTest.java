a1473727ddc694893ae787c1858eb28ba2050575
dorcasndungu <ndungumwihaki017@gmail.com>
2022-03-05 07:35:17 +0300

    final commit

--- src/test/java/model/heroTest.java
+++ src/test/java/model/heroTest.java
@@ -8,40 +8,5 @@ import static org.junit.Assert.*;
 
 public class heroTest {
 
-    @Test
-    public void getName() {
-    Hero testHero=newHero();
-    assertEquals("pu",testHero.getName());
-    }
 
-    @Test
-    public void getAge() {
-        Hero testHero=newHero();
-        assertEquals(23,testHero.getAge());
-    }
-
-    @Test
-    public void getSpecial_power() {
-        Hero testHero=newHero();
-        assertEquals("sing",testHero.getSpecial_power());
-    }
-
-    @Test
-    public void getWeakness() {
-        Hero testHero=newHero();
-        assertEquals("mad",testHero.getWeakness());
-    }
-
-    @Test
-    public void returnHeroes() {
-        Hero testHero=newHero();
-        ArrayList<Hero>testHeroes=new ArrayList<>();
-        testHeroes.add(testHero);
-        assertEquals(testHeroes.contains(testHero), Hero.getAll().contains(testHero)
-        );
-    }
-    //tested
-    public Hero newHero(){
-        return new Hero("pu",23,"sing","mad");
-    }
 }
--- src/test/java/model/teamTest.java
+++ src/test/java/model/teamTest.java
@@ -1,30 +1,11 @@
 package model;
 
+import models.Team;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class teamTest {
 
-    @Test
-    public void getSize() {
-      Team testTeam =  newSquad();
-      assertEquals(3, testTeam.getSize());
-    }
 
-    @Test
-    public void getName() {
-        Team testTeam =  newSquad();
-        assertEquals("than", testTeam.getName());
-    }
-
-    @Test
-    public void getCause() {
-        Team testTeam =  newSquad();
-        assertEquals("help", testTeam.getCause());
-    }
-    //tested
-    public Team newSquad(){
-        return new Team(3,"than","help");
-    }
 }
